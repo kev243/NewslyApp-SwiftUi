743b423a23a7935a32959240034a5b054e8de178
Kevin Nimi <kevin.nimi@uqtr.ca>
2024-03-22 00:24:05 -0400

    Initial Commit

---
+++ Newsly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Newsly
+//
+//  Created by Kevin Nimi on 2024-03-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Newsly/NewslyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewslyApp.swift
+//  Newsly
+//
+//  Created by Kevin Nimi on 2024-03-22.
+//
+
+import SwiftUI
+
+@main
+struct NewslyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
